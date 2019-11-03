5f91c8512fa6a8dc499ed14695f904428e82d74a
Jake Nelson <j.n@live.com.au>
2019-11-03 09:04:23 +0800

    Initial Commit

---
+++ CoreData_from_JSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreData_from_JSON
+//
+//  Created by Jake Nelson on 3/11/19.
+//  Copyright © 2019 Jake Nelson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
